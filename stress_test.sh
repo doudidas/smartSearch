3d908b9749b13546027dfe6ce1f2acd8bb7ab9e8
Edouard Topin <ed.topin@gmail.com>
2018-06-24 16:58:47 +0200

    format change

--- stress_test.sh
+++ stress_test.sh
@@ -1,25 +1,25 @@
-curl -s "http://yourapp.com/page{1, 2}.php?[1-1000]" &
-pidlist="$pidlist $!" 
-curl -s "http://yourapp.com/page{1, 2}.php?[1-1000]" &
-pidlist="$pidlist $!" 
-curl -s "http://yourapp.com/page{1, 2}.php?[1-1000]" &
-pidlist="$pidlist $!" 
-curl -s "http://yourapp.com/page{1, 2}.php?[1-1000]" &
-pidlist="$pidlist $!" 
-curl -s "http://yourapp.com/page{1, 2}.php?[1-1000]" &
-pidlist="$pidlist $!" 
-curl -s "http://yourapp.com/page{1, 2}.php?[1-1000]" &
-pidlist="$pidlist $!" 
-curl -s "http://yourapp.com/page{1, 2}.php?[1-1000]" &
-pidlist="$pidlist $!"  
+curl -s "https://localhost/topic?[1-1000]" &
+pidlist="$pidlist $!"
+curl -s "https://localhost/topic?[1-1000]" &
+pidlist="$pidlist $!"
+curl -s "https://localhost/topic?[1-1000]" &
+pidlist="$pidlist $!"
+curl -s "https://localhost/topic?[1-1000]" &
+pidlist="$pidlist $!"
+curl -s "https://localhost/topic?[1-1000]" &
+pidlist="$pidlist $!"
+curl -s "https://localhost/topic?[1-1000]" &
+pidlist="$pidlist $!"
+curl -s "https://localhost/topic?[1-1000]" &
+pidlist="$pidlist $!"
 
-for job in $pidlist do 
-  echo $job     
-  wait $job || let "FAIL+=1" 
-done  
+for job in $pidlist do
+  echo $job
+  wait $job || let "FAIL+=1"
+done
 
-if [ "$FAIL" == "0" ]; then 
-  echo "YAY!" 
-else 
-  echo "FAIL! ($FAIL)" 
+if [ "$FAIL" == "0" ]; then
+  echo "YAY!"
+else
+  echo "FAIL! ($FAIL)"
 fi
---
+++ test.rb
@@ -0,0 +1,2 @@
+require 'watir'
+Watir::Browser.new :firefox, url: "https://localhost"
